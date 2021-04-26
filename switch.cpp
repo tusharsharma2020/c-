62b3d4466cdd3c128d5f324f67eb01dda2c56049
tusharsharma2020 <82823944+tusharsharma2020@users.noreply.github.com>
2021-04-26 20:55:22 +0530

    basics programs

---
+++ switch.cpp
@@ -0,0 +1,39 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+    char colour;
+    cout<<"enter the colour name";
+    cin>>colour;
+    switch(colour)
+    {
+    case 'r':
+        cout<<"red";
+        break;
+    case 'w':
+        cout<<"white";
+        break;
+    case 'o':
+        cout<<"orange";
+        break;
+    case 'y':
+        cout<<"yellow";
+        break;
+    case 'b':
+        cout<<"black";
+        break;
+        
+    }
+
+    
+
+    return 0;
+}
