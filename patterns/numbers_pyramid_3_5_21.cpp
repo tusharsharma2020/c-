b87fdc42d07035962ed1190af0c3dc4bc13d4cc8
tusharsharma2020 <82823944+tusharsharma2020@users.noreply.github.com>
2021-05-09 07:54:58 +0530

    another one

---
+++ patterns/numbers_pyramid_3_5_21.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int rows;
+    cout<<"enter the rows";
+    cin>>rows;
+
+    for( int i=1; i<=rows;i++)
+        {
+            for ( int j=1;j<=rows-i;j++)
+                {
+                    cout<<" ";
+                }
+            for(int k=1;k<=i;k++)
+                {
+                    cout<<k<<" ";
+                }
+
+            cout<<"\n";
+        }
+
+        return 0;
+}
